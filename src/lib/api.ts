846d47ba26e9224c9ba38f0d3b28df1bbb07dfcf
Yarin Suissa <117517623+YarinSuissa@users.noreply.github.com>
2025-08-15 12:52:42 +0300

    Deleted api.ts

--- src/lib/api.ts
+++
@@ -1,4 +0,0 @@
-import { log } from '../utils/logger'
-
-// Legacy API functions removed - now using secure Edge Function-based approach
-// All file operations now go through secureApi.ts with server-side signing
